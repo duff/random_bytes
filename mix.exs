1909398cb2f71278f4eb1d2496f8ce637a7f967e
Duff O'Melia <duff@omelia.org>
2018-10-26 07:02:51 -0400

    Use the latest ex_doc

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule RandomBytes.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
     ]
   end
 
